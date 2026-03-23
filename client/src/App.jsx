fa147c821b49b7d6c4234b4a9ae84e58990fd363
UZMABEGUM SHAIK <uzmabegum0306@gmail.com>
2026-03-24 01:27:44 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Siginin from "./pages/Siginin";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 export default function App() {
   return (
-   <h1 className='text-3xl text-red-600'>Uzma's Blog</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />{" "}
+        <Route path="/sign-in" element={<Siginin />} />
+        <Route path="/sign-up" element={<SignUp />} />{" "}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
